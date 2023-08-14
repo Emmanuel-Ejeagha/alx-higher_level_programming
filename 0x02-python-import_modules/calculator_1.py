895ab99f8e1e99d06b27e25279f031bd99d20b5d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-08-14 21:29:56 +0000

    python-import_modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addiction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """my subtraction function
+
+    Args:
+        a: first value
+        b: second value
+	
+    Returns:
+		The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first value
+        b: second value
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """This is my division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+	"""
+    return (a / b)
