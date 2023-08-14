6bf1067a8101146551b4d81422d5b8ee8c51c8f5
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-08-14 04:06:57 +0000

    python import and modules

---
+++ 0x01-python-if_else_loops_functions/0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Arg:
+        a: first integer
+	b: second integar
+
+    Returns:
+    	The return value. a + b
+    """
+    return (a + b)
