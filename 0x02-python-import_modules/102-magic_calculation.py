3dc1fe565c7d45e425e7fcbaf32efa8adc749bf8
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-08-15 04:59:42 +0000

    python

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+    if (a < b):
+        c = add(a, b)
+        for z in range(4, 6):
+            c = add(c, z)
+        return(c)
+    return(sub(a, b))
+~
+~
+~
+
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
-    argv = sys.argv[1:]
+    argv = sys.argv
+    res = 0
 
-    if argc == 0:
-        print(") arguments.")
-    elif argc == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(argc))
+    for i in range(1, argc):
+        res = res + int(argv[i])
 
-    for i, arg in enumerate(argv):
-        print(f"{i + 1}: {arg}")
+    print(res)
+~
+
