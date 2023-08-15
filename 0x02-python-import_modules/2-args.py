5b5b5c8dd842df0d8aa0b9b24b2613c7fe70bf4b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-08-15 21:03:41 +0000

    Python import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     if (len(sys.argv) == 1):
         print("0 arguments.")
-    elif(len(sys.argv) == 2):
+    elif (len(sys.argv) == 2):
         print("{} argument:".format(len(sys.argv)-1))
     else:
         print("{} arguments:".format(len(sys.argv)-1))
